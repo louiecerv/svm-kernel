7c1ed29116cc3575d35e60ded52db3cf2f15dcc8
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-01 10:50:18 +0800

    Save

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns; sns.set()
-from sklearn import svm
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
